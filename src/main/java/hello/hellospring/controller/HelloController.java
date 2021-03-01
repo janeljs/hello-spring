73e621c79218fc07ba35a8be6438573b9cb2d5e8
janeljs <jisunlim818@gmail.com>
2021-03-02 02:42:41 +0900

    회원 관리 예제

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,6 +19,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
 
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body에 내용을 직접 넣어주겠다. 페이지 소스보기 하면 hello Jane이 그대로 나옴
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체를 넘겨주면 JsonConverter가 json 방식으로 데이터를 만들어서 반환 (vs. XML(HTML))
+    }
+
+    static class Hello {
+        private String name;
+
+        // property 접근 방식
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
