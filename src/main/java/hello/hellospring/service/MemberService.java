ce16ace0958d8a48be8d38c8bb896eeab8466b0e
janeljs <jisunlim818@gmail.com>
2021-03-03 03:33:47 +0900

    JPA 실습

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Optional;
 // 서비스는 비즈니스 의존적인 설계를 해야함 (메서드 명)
 // 기획자와 소통
  // 스프링이 멤버서비스를 알아보고 등록해줌, service도 component
+@Transactional // 데이터를 저장, 변경하려면 transactional이 있어야함
 public class MemberService {
 
     private final MemberRepository memberRepository;
